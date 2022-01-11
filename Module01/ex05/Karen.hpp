497bfc594d8fa6b4a8216e2ebfe4f2bc1c647cd5
Charlie <charlierdlp@gmail.com>
2022-01-11 18:27:21 +0100

    ex04/05

---
+++ Module01/ex05/Karen.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <iostream>
+
+class Karen
+{
+	private:
+	
+		void  	debug();
+		void  	info();
+		void	warning();
+		void 	error();
+
+	public:
+			
+		Karen();
+		~Karen();
+
+		void	complain(std::string message);
+};
