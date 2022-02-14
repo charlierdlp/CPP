2a3caa57e3a369bb0fc3a0e51db351fdc66386ed
Charlie <charlierdlp@gmail.com>
2022-02-14 01:04:21 +0100

    working on Character class

--- Module04/ex03/Character.cpp
+++ Module04/ex03/Character.cpp
@@ -7,5 +7,32 @@ Character::Character() : _name("")
 
 Character::Character(std::string const & name) : _name(name)
 {
+	for (int i = 0; i < 4; i++)
+		this->_inventory[i] = NULL;
+}
 
+Character::Character(Character const &copy)
+{
+	this->_name = copy._name;
+	for (int i = 0; i < 4; i++)
+		this->_inventory[i] = copy._inventory[i];
+}
+
+Character::~Character()
+{
+	for (int i = 0; i < 4; i++)
+		delete this->_inventory[i];
+}
+
+Character & Character::operator=(Character const &character)
+{
+	this->_name = character._name;
+	for (int i = 0; i < 4; i++)
+		this->_inventory[i] = character._inventory[i];
+	return *this;
+}
+
+std::string const & Character::getName() const
+{
+	return this->_name;
 }
