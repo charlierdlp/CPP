e29297460f892de3a987abd204104b84018b1e27
Charlie <charlierdlp@gmail.com>
2022-03-08 22:32:16 +0100

    Module08: working on ex01, ex00 needs testing

---
+++ Module08/ex01/span.cpp
@@ -0,0 +1,42 @@
+#include "span.hpp"
+
+Span::Span(unsigned int N) : _N(N)
+{
+	_vector.reserve(N);
+}
+
+Span::Span(Span const &src) : _N(src._N), _vector(src._vector)
+{
+}
+
+Span &Span::operator=(Span const &span)
+{
+	if (this != &span)
+	{
+		_N = span._N;
+		_vector = span._vector;
+	}
+	return *this;
+}
+
+void Span::addNumber(int n)
+{
+	if (_vector.size() < _N)
+		_vector.push_back(n);
+	else
+		throw Span::SpanFullException();
+}
+
+int Span::shortestSpan()
+{
+	if (_vector.size() < 2)
+		throw Span::SpanEmptyException();
+	int shortest = _vector[1] - _vector[0];
+	for (unsigned int i = 0; i < _vector.size() - 1; i++)
+	{
+		int span = _vector[i + 1] - _vector[i];
+		if (span < shortest)
+			shortest = span;
+	}
+	return shortest;
+}
---
+++ Module08/ex01/span.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <vector>
+
+class Span
+{
+	private:
+		unsigned int _N;
+		std::vector<int> _vector;
+
+	public:
+		Span(unsigned int N);
+		Span(Span const &src);
+		~Span();
+
+		Span &operator=(Span const &rhs);
+
+		void addNumber(int n);
+		int shortestSpan();
+		int longestSpan();
+	
+	class SpanFullException : public std::exception
+	{
+		public:
+			virtual const char *what() const throw()
+			{
+				return ("Vector is full!");
+			}
+	};
+
+	class SpanEmptyException : public std::exception
+	{
+		public:
+			virtual const char *what() const throw()
+			{
+				return ("Vector is empty or has only one element!");
+			}
+	};
+};
