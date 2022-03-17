6ef9dbba341e6d35cc2d184517fedb69b5a2e03d
Charlie <charlierdlp@gmail.com>
2022-03-17 16:41:26 +0100

    finished

--- Module08/ex01/main.cpp
+++ Module08/ex01/main.cpp
@@ -14,22 +14,47 @@
 
 int main()
 {
-	std::vector<int> vector;
-	Span sp = Span(5);
+	Span sp = Span(10);
+
+	try
+	{
+		std::cout << sp.shortestSpan() << std::endl;
+	}
+	catch(Span::SpanEmptyException &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+
 	sp.addNumber(5);
 	sp.addNumber(3);
 	sp.addNumber(17);
 	sp.addNumber(9);
 	sp.addNumber(11);
+	sp.addNumber(6);
+	sp.addNumber(1);
+	sp.addNumber(7);
+	sp.addNumber(90);
+	sp.addNumber(22);
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
 
+	try
+	{
+		sp.addNumber(42);
+	}
+	catch(Span::SpanFullException &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	
+
+	std::vector<int> vector;
 	for (int i = 0; i < 10042; i++)
 		vector.push_back(i);
 	Span sp2 = Span(vector.size());
 	sp2.addRange(vector.begin(), vector.end());
 
-	//std::cout << sp2.shortestSpan() << std::endl;
-	//std::cout << sp2.longestSpan() << std::endl;
+	std::cout << sp2.shortestSpan() << std::endl;
+	std::cout << sp2.longestSpan() << std::endl;
 
 }
--- Module08/ex02/main.cpp
+++ Module08/ex02/main.cpp
@@ -17,15 +17,19 @@ int main()
 {
 	MutantStack<int> mstack;
 	mstack.push(5);
-	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
-	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	mstack.push(4);
 	mstack.push(3);
-	mstack.push(5);
-	mstack.push(737);
-	//[...]
+	mstack.push(2);
+	mstack.push(1);
 	mstack.push(0);
+	std::cout << "Top element: " << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << "Size: " << mstack.size() << std::endl;
+	std::cout << "Top element: " << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << "Size: " << mstack.size() << std::endl;
+
+
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
@@ -35,6 +39,12 @@ int main()
 	std::cout << *it << std::endl;
 	++it;
 	}
+	std::cout << std::endl;
 	std::stack<int> s(mstack);
+	while (!s.empty())
+	{
+		std::cout << s.top() << std::endl;
+		s.pop();
+	}
 	return 0;
 }
